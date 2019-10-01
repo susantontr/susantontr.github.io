365fa338f564c5be2de61e3db8df0964953c791e
Susan Tang <susantonsz@hotmail.com>
2019-10-01 11:56:36 +0800

    +javascript

--- index.js
+++ index.js
@@ -12,7 +12,33 @@ $(".yoga").hover(function(){
 
 
 
+$(".fas1").hover(function(){
+  $(this).css("color", "#202040");
+});
+$(".fas1").click(function(){
+  $(".p2").css("color", "black");
+});
 
+$(".fas2").hover(function(){
+  $(this).css("color", "#204969");
+});
+$(".fas2").click(function(){
+  $(".p3").css("color", "black");
+});
+
+$(".fas3").hover(function(){
+  $(this).css("color", "#00bdaa");
+});
+$(".fas3").click(function(){
+  $(".p4").css("color", "black");
+});
+
+$(".fas4").hover(function(){
+  $(this).css("color", "#347474");
+});
+$(".fas4").click(function(){
+  $(".p5").css("color", "black");
+});
 
 
 
